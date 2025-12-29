5b57f7e6198db371249ad952d967d99e99ba81f0
Alexander Ivanov <alex.ivanov0722@gmail.com>
2025-12-29 23:40:29 +0200

    finish split

--- src/tripadvisor_dataset/tripadvisor_dataset_setup.py
+++ src/tripadvisor_dataset/tripadvisor_dataset_setup.py
@@ -1,8 +1,10 @@
 from pathlib import Path
 import kagglehub
 import pandas as pd
+from sklearn.model_selection import train_test_split
+
 from tripadvisor_dataset_settings import (
-    DATASET_FILE_NAME, DATASET_NAME, NEG_RATINGS, POS_RATINGS, TEXT_COL, RATING_COL
+    DATASET_FILE_NAME, DATASET_NAME, NEG_RATINGS, POS_RATINGS, RANDOM_STATE, TEST_SIZE, TEXT_COL, RATING_COL
 ) 
 
 
@@ -40,12 +42,50 @@ def to_binary_labels(df: pd.DataFrame) -> pd.DataFrame:
     return df[[TEXT_COL, "label"]]
 
 
-# def split_train_val_test(df: pd.DataFrame):
+def split_train_test(df: pd.DataFrame):
+    """Split dataset in train and test data
+
+    Args:
+        df (pd.DataFrame): data to split
+
+    Returns:
+        _type_: tuple of train,test dataset split
+    """
     
+    x_train, x_test, y_train, y_test = train_test_split(
+        df[TEXT_COL], df["label"],
+        test_size=TEST_SIZE,
+        random_state=RANDOM_STATE,
+        stratify=df["label"]
+    )
+    return (x_train, y_train), (x_test, y_test)
+
+
+def save_splits(train, test):
+    """Saves the splits on the disk
+
+    Args:
+        train (_type_): train dataframe
+        test (_type_): test dataframe
+    """
+    project_dir = Path(__file__).resolve().parents[1]
+    processed_data_path = project_dir / "data"
+    processed_data_path.mkdir(parents=True, exist_ok=True)
+
+    (X_train, y_train) = train
+    (X_test, y_test) = test
+
+    pd.DataFrame({TEXT_COL: X_train, "label": y_train}).to_csv(processed_data_path / "train.csv", index=False)
+    pd.DataFrame({TEXT_COL: X_test, "label": y_test}).to_csv(processed_data_path / "test.csv", index=False)
+
 
 def main():
     df = load_dataset_from_csv()
-    df = to_binary_labels
+    df = to_binary_labels(df)
+    
+    train, test = split_train_test(df)
+    save_splits(train, test)
+
 
 if __name__ == "__main__":
     main()
